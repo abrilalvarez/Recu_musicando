e32339c9621ad7e69708588765d2e6222212972a
abrilalvarez <abriinahir1@gmail.com>
2023-03-10 15:41:02 -0300

    modificación en dump indicando creacion de la db: correccion error 'no database selected'

--- db/musicando.sql
+++ db/musicando.sql
@@ -4,6 +4,11 @@
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.4.17-MariaDB
 
+DROP DATABASE IF EXISTS  `musicando`;
+CREATE DATABASE `musicando` CHARACTER SET 'latin1' COLLATE 'latin1_spanish_ci';
+
+USE `musicando`;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
